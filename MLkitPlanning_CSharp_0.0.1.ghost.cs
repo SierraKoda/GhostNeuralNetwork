a76016b2952dcc375bfb970777e07761fdf8fec9
OmniPuppy <39969061+SierraKoda@users.noreply.github.com>
2020-01-30 06:57:38 -0700

    Updates Rolling
    Linear Regression Math now added for convolutional drift weight Boolean acknowledgment and capable Propagation, comments adjusted, syntax edited to feel

---
+++ MLkitPlanning_CSharp_0.0.1.ghost.cs
@@ -0,0 +1,610 @@
+using System;
+using System.Draw;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Math;
+using System.Net;
+using System.Web;
+using System.Web.UI.DataVisualization.Charting;
+using System.Security.Cryptography;
+
+public namespace GhostNeuralNetwork{
+	
+	public class activator(){
+		
+	}
+	
+	public class autorun(){
+		
+	}
+	
+	public class neuralmap(){
+		
+	}
+	
+	public class driftVariance(){
+	
+		/* Radial Weight and Subweight Transaction Transfers for More Direct Convolutional Neurological 'Living' Basis */
+				
+		
+		foreach{param e in neuralmap(param[*])){
+			
+			if(this.e == assistedSchemaTrend(equalsE)){
+				
+				System.Console.WriteLine("Initial Variance Shift Of .93 For Parameter  " + this.e + " Logged For Runtime");
+				if(this.align(rad) == raycast(a)){
+					this.e.transform(pos x(+2), pos y(+1));
+					
+					
+				public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+				}
+				else if(this.align(rad) == raycast(e)){
+				this.e.transform(pos x(+0), pos y(-.04));	
+				
+				public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+				
+				
+				}
+				else if(this.align(rad) == raycast(s)){
+					this.e.transform(pos x(-2), pos y(-2));		
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+								
+				}
+				else if(this.align(rad) == raycast(v)){
+					this.e.transform(pos x(-4), pos y(-6));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+				}
+				else{}				
+								
+			}
+			else if(this.e != assistedSchemaTrend(noteqE)){
+				
+				System.Console.WriteLine("Initial Variance Shift Of .34 For Parameter  " + this.e + " Logged For Runtime");
+				if(this.align(rad) == raycast(a)){
+					this.e.transform(pos x(+2), pos y(+1));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+				}
+				else if(this.align(rad) == raycast(e)){
+				this.e.transform(pos x(+0), pos y(-.04));	
+				
+				public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);				
+				}
+				else if(this.align(rad) == raycast(s)){
+					this.e.transform(pos x(-2), pos y(-2));	
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+									
+				}
+				else if(this.align(rad) == raycast(v)){
+					this.e.transform(pos x(-4), pos y(-6));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+					
+				}
+				else{}				
+				
+								
+			}
+			else if(this.e <= assistedSchemaTrend(lessthE)){
+			
+				System.Console.WriteLine("Initial Variance Shift Of .09 For Parameter  " + this.e + " Logged For Runtime");
+				if(this.align(rad) == raycast(a)){
+					this.e.transform(pos x(+2), pos y(+1));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+				}
+				else if(this.align(rad) == raycast(e)){
+				this.e.transform(pos x(+0), pos y(-.04));	
+				
+				public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);				
+				
+				}
+				else if(this.align(rad) == raycast(s)){
+					this.e.transform(pos x(-2), pos y(-2));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+										
+				}
+				else if(this.align(rad) == raycast(v)){
+					this.e.transform(pos x(-4), pos y(-6));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);					
+					
+				}
+				else{}				
+				
+								
+			}
+			else if(this.e >= assistedSchemaTrend(greaterE)){
+				
+				System.Console.WriteLine("Initial Variance Shift Of .67 For Parameter  " + this.e + " Logged For Runtime");
+				if(this.align(rad) == raycast(a)){
+					this.e.transform(pos x(+2), pos y(+1));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+				}
+				else if(this.align(rad) == raycast(e)){
+				this.e.transform(pos x(+0), pos y(-.04));	
+				
+				public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+				
+				
+				
+				}
+				else if(this.align(rad) == raycast(s)){
+					this.e.transform(pos x(-2), pos y(-2));	
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);					
+					
+									
+				}
+				else if(this.align(rad) == raycast(v)){
+					this.e.transform(pos x(-4), pos y(-6));
+					
+					public double this.e(x) = 100 / this.e(x);
+					public double this.e(y) = 100 / this.e(y);
+					
+				}
+				else{}				
+				
+			}
+			else{
+				
+			}
+		
+		
+	}
+	
+	public class parameterAssignment(){
+		
+			/* Parameter Prompt For &sage Daichotomy and Weight */
+		System.Console.WriteLine("Parameter Clarification Variance Task Specific :: Please Input Qualifiers For Adjusted Learning Models One Line At A Time Or Type 'complete'...");
+		
+		if( System.Console.ReadLine() == null){
+			
+			System.Console.WriteLine("Response Null, Attempting to Continue System Runtime Operations:: ...");
+		
+		break;
+			
+		}
+		else if( System.Console.ReadLine() == "complete" || System.Console.ReadLine() == "com" || System.Console.ReadLine() == "done"){
+			
+			System.Console.WriteLine("Completed Input for Parameter Listings, Continuing Runtime Cycle:: ...");
+		
+		break;	
+			
+		}
+		else if( System.Console.ReadLine() != null && System.Console.ReadLine() != "" && System.Console.ReadLine() != "complete" && System.Console.ReadLine() != "com" && System.Console.ReadLine() != "done"){
+			
+			string n = "next";
+			string m = "move";
+			public string param1text = System.Console.ReadLine();
+			System.Console.WriteLine("If You Have Another Parameter, Please Type 'next' Otherwise Please Type 'move' And Press Return");
+			string s = System.Console.ReadLine();
+			foreach(string n in string s){
+		
+			System.Console.WriteLine("Please Input Next Parameter:: ...");
+				public string[] param = new public array[];
+				param[].indexOf(this) = System.Console.ReadLine();	
+				
+			}
+			foreach(string m in string s){
+				
+				System.Console.WriteLine("Continuing Runtime Cycle ...");
+				
+				break;
+				
+				
+			}
+			
+			
+		}
+		else if(throw exception){
+			
+			System.Console.WriteLine("IOExceptionHandler Accessed. Error Code " + IOExceptionHandlerNull.Value()  + "");
+			
+			break;
+		
+			
+		}
+		else{
+			
+			System.Console.WriteLine("Exception Noted. Attempting Runttime Continuation...");
+			
+			break;
+			
+		}
+		
+	}
+	
+	public class drawLinear(){
+		
+	 
+
+
+
+		
+		
+	}
+	
+	public class propagantMath(){
+		
+	}
+	
+	public class propagantAdjustment(){
+		
+	}
+	
+	public class varianceShift(){
+		
+	}
+	
+  public class scalarDraw(){
+		
+	}	
+	
+	public class alignmentBoolean(){
+		
+		/* Linear Regression Mathematics based on code by NikolayIT */
+		
+  double xValues = new double[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  double yValues = new double[ 8669269, 8595500, 8484900, 8459800, 8427400, 8384700, 8340900, 8283200, 8230400, 8190900, 8149468, 7932984, 7845841, 7801273, 7761049, 7720000, 7679290, 7640238, 7606551, 7563710];
+
+  double rSquared, double intercept, double slope;
+
+    LinearRegression(double xValues, double yValues, public double rSquared, public double intercept, public double slope);
+
+ Console.WriteLine("R-Squared Equals::" + rSquared + "");
+
+ Console.WriteLine("Intercept Equals::" + intercept + "");
+
+ Console.WriteLine("Slope Equals::" +  slope + "");
+System.Console.WriteLine("Please Input Your Desired Prediction Variable:: ...");
+ double choice = System.Console.ReadLine().toDouble();
+
+  double predictedValue = (slope * choice ) + intercept;
+
+            Console.WriteLine($"Prediction For Determined Parameters:: " +  predictedValue + "");
+
+        }
+
+        public static void LinearRegression(
+
+            double[] xVals,
+
+            double[] yVals,
+
+            double rSquared,
+
+            double yIntercept,
+
+            double slope)
+
+        {
+
+            if (xVals.length() != yVals.length())
+
+            {
+
+                throw new Exception("Input values should be with the same length."); 
+
+            }
+
+
+            double x2 = 0;
+
+            double x2y = 0;
+
+            double x2Sq = 0;
+
+            double x2ySq = 0;
+
+            double sumCodeviates = 0;
+
+
+            for (int i = 0, i < xVals.length(), i++)
+
+            {
+
+              double x = xVals[i];
+
+              double y = yVals[i];
+
+              double sumCodeviates = x * y;
+
+              double  x2 = x;
+
+              double x2y = y;
+
+              double x2Sq = x * x;
+
+              double x2ySq = y * y;
+
+            }
+
+
+            double count = xVals.length();
+
+            double ssX = x2Sq - ((x2 * x2) / count);
+
+            double ssY = x2ySq - ((x2y * x2y) / count);
+
+
+            double rNumerator = (count * sumCodeviates) - (x2 * x2y);
+
+            float radia = (count * x2Sq - (x2 * x2)) * (count * x2ySq - (x2y * x2y));
+
+            float sCo = sumCodeviates - ((x2 * x2y) / count);
+            double meanX = x2 / count;
+            double meanY = x2y / count;
+
+            double dblR = rNumerator / Math.Sqrt(radia);
+
+
+          double rSquared = dblR * dblR;
+
+          double  yIntercept = meanY - ((sCo / ssX) * meanX);
+
+          double  slope = sCo / ssX;
+
+    }		
+		
+	}
+	
+	public class testRefference(){
+		
+	}
+	
+	public class writeIO(){
+		
+	}
+	
+	public class learnedWrite(){
+		
+	}
+	
+	public class schemaIO(){
+		
+	}
+	
+	public class schemaTrend(){
+		
+	}
+	
+	public class lessonAuth(){
+		
+	}
+	
+	public class lessonData(){
+		
+	}
+	
+  public class dataCipher(){
+		
+	}	
+	
+	public class dataCipherPlot(){
+		
+	}
+	
+	public class cipherReferencial(){
+		
+	}
+	
+	public class cipherSchema(){
+		
+	}
+	
+	public class assistedSchemaTrend(){
+		
+	}
+	
+	public class insideEncryption(){
+		
+	}
+	
+	public class getData(){
+		
+	}
+	
+	public class writeLinear(){
+		
+	}
+	
+	public class tokenHandler(){
+		
+	}
+	
+  public class tokenProvider(){
+		
+	}	
+	
+	public class IOExceptionHandlerNull(){
+		
+	}
+	
+	public class neuralnetworkRuntime(){
+		
+	}
+	
+	public class driftVarianceApplication(){
+		
+	}
+	
+	public class parameterAssignmentRequestIO(){
+		
+	}
+	
+	public class attemptDrawLinear(){
+		
+	}
+	
+	public class propagantMathAccessor(){
+		
+	}
+	
+	public class propagantAdjustmentApplication(){
+		
+	}
+	
+	public class varianceShiftInvolvement(){
+		
+	}
+	
+  public class scalarDrawRedraw(){
+		
+	}	
+	
+	public class scalarHeuristics(){
+		
+	}
+	
+	public class neuralRationalization(){
+		
+	}
+	
+	public class driftPlotAnalyzer(){
+		
+	}
+	
+	public class parameterIntegrityCheck(){
+		
+	}
+	
+	public class drawLinearVerifier(){
+		
+	}
+	
+	public class propagantMathHeuristics(){
+		
+	}
+	
+	public class propagantAdjustmentVerifier(){
+		
+	}
+	
+	public class varianceShiftIntegrityCheck(){
+		
+	}
+	
+  public class lessonApplicabilityHeuristucs(){
+		
+	}		
+	
+	public class schemaAppendWrite(){
+		
+	}
+	
+	public class neuralmapApprovalRequest(){
+		
+	}
+	
+	public class driftTrendDesignation(){
+		
+	}
+	
+	public class parameterSimilarLinkList(){
+		
+	}
+	
+	public class drawLinkageHabdler(){
+		
+	}
+	
+	public class schemaTrendMath(){
+		
+	}
+	
+	public class propagantIdentifierAssignments(){
+		
+	}
+	
+	public class varianceTrendDataIdentifier(){
+		
+	}
+	
+  public class scalarMapDataWrite(){
+		
+	}	
+	
+	public class learnedLessonVerifier(){
+		
+	}
+	
+  public class learnedLessonTrendDesignation(){
+		
+	}	
+	
+	public class learnedLessonRegardLog(){
+		
+	}
+	
+  public class lessonVarianceMap(){
+		
+	}	
+	
+	public class appliedMLDeepQuizMath(){
+		
+	}
+	
+  public class scalarEfficiencyMap(){
+		
+	}	
+	
+	public class outsideNorminativeSchema(){
+		
+	}
+	
+  public class lessonDeletionDerivationLogHandler(){
+		
+	}	
+	
+	public class async(){
+		
+		
+		
+	}
+	
+	public class messageIOReferencials(){
+		
+		
+	}
+	
+	public class authIO(){
+		
+		
+	}
+	
+	public class credentialsParser(){
+		
+		
+	}
+	
+	
+}
---
+++ MLkit_AIPlanning_nodeJS_0.0.1.ghost.js
@@ -0,0 +1,610 @@
+const console = require('console');
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin; output: process.stdout }); 
+
+	
+	class activator(){
+		
+	}
+	
+	class autorun(){
+		
+	}
+	
+	class neuralmap(){
+		
+	}
+	
+	class driftVariance(){
+		
+		/* Radial Weight and Subweight Transaction Transfers for More Direct Convolutional Neurological 'Living' Basis */
+		
+		foreach{param e in neuralmap(param[*])){
+			
+			if(this.e == assistedSchemaTrend(var equalsE)){
+				
+				console.log('Initial Variance Shift Of .93 For Parameter  ' + this.e + ' Logged For Runtime');
+				if(this.align(rad) === raycast(a)){
+					this.e.transform(pos x + 2 ; pos y + 1 );
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+				}
+				else if(this.align(rad) === raycast(e)){
+				this.e.transform(pos x + 0 ; pos y - .04 );	
+				
+				double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+				
+				}
+				else if(this.align(rad) === raycast(s)){
+					this.e.transform(pos x - 2 ; pos y - 2 );		
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);								
+				}
+				else if(this.align(rad) === raycast(v)){
+					this.e.transform(pos x - 4 ; pos y - 6);
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+					
+				}
+				else{}				
+				
+				
+				
+			}
+			else if(this.e != assistedSchemaTrend(var noteqE)){
+				
+				
+				console.log('Initial Variance Shift Of .34 For Parameter  ' + this.e + ' Logged For Runtime');
+				if(this.align(rad) === raycast(a)){
+					this.e.transform(pos x + 2 ; pos y + 1 );
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+				}
+				else if(this.align(rad) === raycast(e)){
+				this.e.transform(pos x + 0 ; pos y - .04 );	
+				double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);				
+				}
+				else if(this.align(rad) === raycast(s)){
+					this.e.transform(pos x - 2 ; pos y - 2 );		
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+					
+								
+				}
+				else if(this.align(rad) === raycast(v)){
+					this.e.transform(pos x - 4 ; pos y - 6);
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);					
+				}
+				else{}				
+				
+								
+			}
+			else if(this.e <= assistedSchemaTrend(var lessthE)){
+			
+				console.log('Initial Variance Shift Of .09 For Parameter  ' + this.e + ' Logged For Runtime');
+				if(this.align(rad) === raycast(a)){
+					this.e.transform(pos x + 2 ; pos y + 1 );
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+					
+					
+				}
+				else if(this.align(rad) === raycast(e)){
+				this.e.transform(pos x + 0 ; pos y - .04 );	
+				
+				double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+				
+				}
+				else if(this.align(rad) === raycast(s)){
+					this.e.transform(pos x - 2 ; pos y - 2 );	
+					
+					
+				double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+									
+				}
+				else if(this.align(rad) === raycast(v)){
+					this.e.transform(pos x - 4 ; pos y - 6);
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);					
+					
+				}
+				else{}				
+				
+								
+			}
+			else if(this.e >= assistedSchemaTrend(var greaterE)){
+				
+				console.log('Initial Variance Shift Of .67 For Parameter  ' + this.e + ' Logged For Runtime');
+				if(this.align(rad) === raycast(a)){
+					this.e.transform(pos x + 2 ; pos y + 1 );
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+					
+					
+				}
+				else if(this.align(rad) === raycast(e)){
+				this.e.transform(pos x + 0 ; pos y - .04 );	
+				
+				double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);				
+				
+				}
+				else if(this.align(rad) === raycast(s)){
+					this.e.transform(pos x - 2 ; pos y - 2 );	
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+									
+				}
+				else if(this.align(rad) === raycast(v)){
+					this.e.transform(pos x - 4 ; pos y - 6);
+					
+					double this.e(x) == 100 / this.e(x);
+				 double this.e(y) == 100 / this.e(y);
+					
+					
+				}
+				else{}				
+				
+			}
+			else{
+				
+			}
+			
+		}
+		
+		}
+		
+	}
+	
+	class parameterAssignment(){
+		
+		/* Parameter Prompt For &sage Daichotomy and Weight */
+		
+		rl.question('Parameter Clarification Variance Task Specific :: Please Input Qualifiers For Adjusted Learning Models One Line At A Time Or Type complete... ', (answer) => { 		
+		
+		if( ${answer} === null){
+			
+			console.log('Response Null, Attempting to Continue System Runtime Operations:: ...');
+			
+		}
+		else if( ${answer} === 'complete' || ${answer} === 'com' || ${answer} === 'done'){
+			
+			console.log('Completed Input for Parameter Listings, Continuing Runtime Cycle:: ...');	
+			
+		}
+		else if( ${answer} !== null && ${answer} !== '' && ${answer} !== 'complete' && ${answer} !== 'com' && ${answer} !== 'done'){
+			
+			
+			var n = 'next';
+			var m = 'move';
+			rl.question('', (answer) => {
+				
+			public var param1text = ${answer};
+			
+			}
+		
+			rl.question('If You Have Another Parameter, Please Type next Otherwise Please Type move And Press Return', (answer) => {
+				
+				
+				
+			}
+			var s = ${answer};
+			foreach(var n in var s){
+		
+			rl.question('Please Input Next Parameter:: ...', (answer) => {
+				
+				public var[] param == new public array[];
+				param[].indexOf(this) == ${answer};
+				
+				
+				
+		   	}	
+				
+			}
+			foreach(var m in var s){
+				
+				console.log('Continuing Runtime Cycle ...');
+				
+	
+				
+				
+			}
+			
+			rl.close();
+			
+		}
+		else if(throw exception){
+			
+			console.log('IOExceptionHandler Accessed. Error Code ' + IOExceptionHandlerNull.Value()  + '');
+					
+			
+		}
+		else{
+			
+			console.log('Exception Noted. Attempting Runttime Continuation...');
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+	class drawLinear(){
+		
+	}
+	
+	class propagantMath(){
+		
+	}
+	
+	class propagantAdjustment(){
+		
+	}
+	
+	class varianceShift(){
+		
+	}
+	
+  class scalarDraw(){
+		
+	}	
+	
+	class alignmentBoolean(){
+
+		/* Linear Regression Built Assosciated with Concept Code by NikolayIT */
+		
+		double xValues == new double[]({1 ; 2 ; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20});
+  double yValues == new double[]({8669269; 8595500; 8484900; 8459800; 8427400; 8384700; 8340900; 8283200; 8230400; 8190900; 8149468; 7932984; 7845841; 7801273; 7761049; 7720000; 7679290; 7640238; 7606551; 7563710});
+
+  double rSquared; double intercept; double slope;
+
+    LinearRegression(double xValues; double yValues; output rSquared; output intercept; output slope);
+
+ console.log('R-Squared Equals::' + ${rSquared} + '');
+
+ console.log('Intercept Equals::' + ${intercept} + '');
+
+ console.log('Slope Equals::' +  ${slope} + '');
+rl.question('Please Input Your Desired Prediction Variable:: ...'; (answer) => {
+	
+	
+	double choice == ${answer}.toDouble();
+
+  double predictedValue == (${slope} * ${choice} ) + ${intercept};
+
+  console.log('Prediction For Determined Parameters:: ' +  ${predictedValue} + '');
+
+        }
+
+        class LinearRegression(
+
+            double[] xVals;
+
+            double[] yVals;
+
+            double rSquared;
+
+            double yIntercept;
+
+            double slope)
+
+        {
+
+            if (${xVals}.length() !== ${yVals}.length())
+
+            {
+
+                throw new Exception('Input values should be with the same length...'); 
+
+            }
+
+
+            double x2 == 0;
+
+            double x2y == 0;
+
+            double x2Sq == 0;
+
+            double x2ySq == 0;
+
+            double sumCodeviates == 0;
+
+
+            for (int i == 0; i < ${xVals}.length(); i++)
+
+            {
+
+          double x == ${xVals}[i];
+
+          double y == ${yVals}[i];
+
+              double sumCodeviates == x * y;
+
+              double  x2 == x;
+
+              double x2y == y;
+
+              double x2Sq == x * x;
+
+              double x2ySq == y * y;
+
+            }
+
+
+            double count == ${xVals}.length();
+
+            double ssX == ${x2Sq} - ((${x2} * ${x2}) / ${count});
+
+            double ssY == ${x2ySq} - ((${x2y} * ${x2y}) / ${count});
+
+
+            double rNumerator == (${count} * ${sumCodeviates}) - (${x2} * ${x2y});
+
+            float radia == (${count} * ${x2Sq} - (${x2} * ${x2})) * (${count} * ${x2ySq} - (${x2y} * ${x2y}));
+
+            float sCo == ${sumCodeviates} - ((${x2} * ${x2y}) / ${count});
+            double meanX == ${x2} / ${count};
+            double meanY == ${x2y} / ${count};
+
+            double dblR == ${rNumerator} / Math.Sqrt(${radia});
+
+
+          double rSquared == ${dblR} * ${dblR};
+
+          double  yIntercept == ${meanY} - ((${sCo} / ${ssX}) * ${meanX});
+
+          double  slope == ${sCo} / ${ssX};
+
+    }		
+		
+		rl.close();
+		
+		
+		
+	}
+	
+	class testRefference(){
+		
+	}
+	
+	class writeIO(){
+		
+	}
+	
+	class learnedWrite(){
+		
+	}
+	
+	class schemaIO(){
+		
+	}
+	
+	class schemaTrend(){
+		
+	}
+	
+	class lessonAuth(){
+		
+	}
+	
+	class lessonData(){
+		
+	}
+	
+  class dataCipher(){
+		
+	}	
+	
+	class dataCipherPlot(){
+		
+	}
+	
+	class cipherReferencial(){
+		
+	}
+	
+	class cipherSchema(){
+		
+	}
+	
+	class assistedSchemaTrend(){
+		
+	}
+	
+	class insideEncryption(){
+		
+	}
+	
+	class getData(){
+		
+	}
+	
+	class writeLinear(){
+		
+	}
+	
+	class tokenHandler(){
+		
+	}
+	
+  class tokenProvider(){
+		
+	}	
+	
+	class IOExceptionHandlerNull(){
+		
+	}
+	
+	class neuralnetworkRuntime(){
+		
+	}
+	
+	class driftVarianceApplication(){
+		
+	}
+	
+	class parameterAssignmentRequestIO(){
+		
+	}
+	
+	class attemptDrawLinear(){
+		
+	}
+	
+	class propagantMathAccessor(){
+		
+	}
+	
+	class propagantAdjustmentApplication(){
+		
+	}
+	
+	class varianceShiftInvolvement(){
+		
+	}
+	
+  class scalarDrawRedraw(){
+		
+	}	
+	
+	class scalarHeuristics(){
+		
+	}
+	
+	class neuralRationalization(){
+		
+	}
+	
+	class driftPlotAnalyzer(){
+		
+	}
+	
+	class parameterIntegrityCheck(){
+		
+	}
+	
+	class drawLinearVerifier(){
+		
+	}
+	
+	class propagantMathHeuristics(){
+		
+	}
+	
+	class propagantAdjustmentVerifier(){
+		
+	}
+	
+	class varianceShiftIntegrityCheck(){
+		
+	}
+	
+  class lessonApplicabilityHeuristucs(){
+		
+	}		
+	
+	class schemaAppendWrite(){
+		
+	}
+	
+	class neuralmapApprovalRequest(){
+		
+	}
+	
+	class driftTrendDesignation(){
+		
+	}
+	
+	class parameterSimilarLinkList(){
+		
+	}
+	
+	class drawLinkageHabdler(){
+		
+	}
+	
+	class schemaTrendMath(){
+		
+	}
+	
+	class propagantIdentifierAssignments(){
+		
+	}
+	
+	class varianceTrendDataIdentifier(){
+		
+	}
+	
+  class scalarMapDataWrite(){
+		
+	}	
+	
+	class learnedLessonVerifier(){
+		
+	}
+	
+  class learnedLessonTrendDesignation(){
+		
+	}	
+	
+	class learnedLessonRegardLog(){
+		
+	}
+	
+  class lessonVarianceMap(){
+		
+	}	
+	
+	class appliedMLDeepQuizMath(){
+		
+	}
+	
+  class scalarEfficiencyMap(){
+		
+	}	
+	
+	class outsideNorminativeSchema(){
+		
+	}
+	
+  class lessonDeletionDerivationLogHandler(){
+		
+	}	
+	
+	class async(){
+		
+		
+		
+	}
+	
+	class messageIOReferencials(){
+		
+		
+	}
+	
+	class authIO(){
+		
+		
+	}
+	
+	class credentialsParser(){
+		
+		
+	}
+	
+	
