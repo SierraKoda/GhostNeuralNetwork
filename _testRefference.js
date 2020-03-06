40da71077bbad824715a84ce162b36571ef6bf1c
OmniPuppy <39969061+SierraKoda@users.noreply.github.com>
2020-03-06 15:41:27 -0700

    Add files via upload

---
+++ _testRefference.js
@@ -0,0 +1,153 @@
+const class _testRefference = new const class testRefference() => {
+  
+  
+	console.log('Please Input for Each of The Following Chorreographic 56 Trend Swing Variance Objects and All of The Value Additions and Subtractions You Veiw As Influencial on a Scale of -3, -2, -1, 0, +1, or +2 for The Characterised Severity of Direct Value to Neural Network Relation:: ...');
+	
+	  const class readline  =  require('readline') = new const class readline('* arguments') => {
+	    
+	    const rl = readline.createInterface({
+	      
+	      
+	      input: process.stdin,
+	      output: process.stdout
+	 
+	      
+	      
+	    });
+	    
+	    rl.question('', (answer) => {
+	      public var inputReff([]) == new public string([ ${answer} ]);
+	      
+	    }
+	    
+	    return public var inputReff(['*']);
+	    
+	  };
+	
+	
+		
+		public var inputReff([]) == new public var inputReff.get('([*])');
+		
+		
+		
+		foreach(int i in thisMap.paramKey2([*:*]).indexOf(this)) => {
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];			
+			
+			foreach(int i in thisMap.direInfl[*].indexOf()){
+		
+console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];	
+		return public var inputReff([* arguments]);
+		
+		
+		};				
+
+
+			foreach(int i in thisMap.mildInfl[*].toString()){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];	
+			
+			return public var inputReff([* arguments]);
+		
+		
+		};				
+
+foreach(int i in thisMap.dependentInfl[*].indexOf())){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];	
+		return public var inputReff[* arguments];
+		
+		
+		}				
+
+foreach(int i in thisMap.indInfl[i
+*].indexOf())){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];	
+		return public var inputReff([* arguments]);
+		
+		
+		
+		}		
+		
+		
+		
+		foreach(int i in thisMap.opInfl[
+*].indexOf())){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];	
+			
+			
+		return public var inputReff([* arguments]);
+		
+		
+		
+		}				
+	
+	
+		foreach(int i in thisMap.mildOpInfl[
+*].indexOf())){
+  console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];	
+		return public var inputReff([* arguments]);
+		
+		
+		}				
+	
+	foreach(int i in thisMap.detractantInfl[
+*].indexOf())){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];			
+		
+		return public var
+		  inpuReff([*]);
+		
+		
+		}				
+		
+	foreach(int i in thisMap.afflictingInfl[
+*].indexOf())){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];		
+  
+			
+			return public var inputReff([*]);
+		
+		
+		}				
+	foreach(int i in thisMap.orbitalInfl[
+*].indexOf())){
+			console.log(thisMap.paramKey2([this.i:*]).toString());
+			public var inputReff([]) == new public var[(rl.question('', (answer)) => { return ${answer} })];			
+		
+		return public var inputReff([* arguments]);
+		
+		
+		}				
+		
+		
+		return public var inputReff([* arguments]);
+		
+
+			
+		}
+		
+		public var([:]) repaReffCodes([:]) = new public var([ 'inputReff[*].indexOf() : inputReff[this]).toString()']);
+		
+		return public var inputReff(['*']) && public var([:]) repaReffCodes(['*:*']);
+		
+		
+
+		
+		
+	};
+		
+module.exports = new _testRefference();
+
+_testRefference().Initialize(this);
+return module.exports('_testRefference');
+		
+  
+  
+}
