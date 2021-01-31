40e8cf6108d9fe3e9ab6daa044acc7cbc2fb715f
OmniPuppy <39969061+SierraKoda@users.noreply.github.com>
2021-01-31 10:34:40 -0600

    Add files via upload

---
+++ _nonEuclidCheck.js
@@ -0,0 +1,23 @@
+const double pos2 = require('_drawLinear.js');
+import pos2 from '_drawLinear'
+new pos2(x,y).value(get(x,y)).eval(this);
+for(pos2(x,y)[*].value(eval(x,y)).exists(true) => {
+const class euclidCoordinate(pos2, Euclid, EuclidMax, nonCheck, assertEuclid) = {}
+euclidCoordinate(pos2(x,y).get(value(x,y).eval()){
+  
+
+const var Euclid(x,y) =  (pos2(x,y)(.205));
+const var EuclidMax(x,y) = new const yPos(140,100);
+const Boolean nonCheck = Boolean(0 < Euclid && Euclid <= 1);
+const Function assertEuclid === nonCheck(true, true);
+for(assertEuclid === true){
+  
+  new const yPos2(x,y) = 100-(100/(100(Euclid(x,y)<(yPos(140,100))))) return yPos2(x,y)
+  
+  } 
+get(yPos2(x,y).value(eval(x,y))).exec()
+}).exec()
+} === Boolean(true, true).exists(true).do{
+  console.log('Heuristic Check Refferences for Draw Position (x,y) All Non-Euclid Equate Truthful Assignment :: ...' + new Date().stringify() + '');
+}
+export(euclidCoordinate(* arguments).eval());
---
+++ _nonEuclidDraw.js
@@ -0,0 +1,21 @@
+const double pos2 = require('_drawLinear.js');
+import pos2 from '_drawLinear'
+new pos2(x,y).value(get(x,y)).eval(this);
+
+const class euclidCoordinate(pos2, Euclid, EuclidMax, nonCheck, assertEuclid) = {}
+euclidCoordinate(pos2(x,y).get(value(x,y).eval()){
+  
+
+const var Euclid(x,y) =  (pos2(x,y)(.205));
+const var EuclidMax(x,y) = new const yPos(140,100);
+const Boolean nonCheck = Boolean(0 < Euclid && Euclid <= 1);
+const Function assertEuclid === nonCheck(true, true);
+for(assertEuclid === true){
+  
+  new const yPos2(x,y) = 100-(100/(100(Euclid(x,y)<(yPos(140,100))))) return yPos2(x,y)
+  
+  } 
+get(yPos2(x,y).value(eval(x,y))).exec()
+}).exec()
+
+export(euclidCoordinate(* arguments).eval());
